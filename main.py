3e1afad6be53db6343b64e1e5822cba9a1873cf2
TariqT777 <TariqThomas777@gmail.com>
2020-12-11 14:29:17 -0500

    Pop up game window is now running until the exit button is pressed.

--- main.py
+++ main.py
@@ -6,4 +6,12 @@ pygame.init()
 height = 800
 width =600
 
+#This creates the game screen
 new_screen = pygame.display.set_mode((height,width))
+
+game_running = True
+
+while game_running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_running = False
